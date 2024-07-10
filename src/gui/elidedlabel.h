ff398b4a05d991d2f4f0714d21c8aa1b1ccbdf9d
Nivas Pvs <sainivaspvs@gmail.com>
2024-07-10 19:33:55 +0530

    Add elidedlabel widget class

---
+++ src/gui/elidedlabel.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <QFrame>
+#include <QString>
+
+class ElidedLabel : public QFrame
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text WRITE setText)
+    Q_PROPERTY(bool isElided READ isElided)
+
+public:
+    explicit ElidedLabel(const QString &text, QWidget *parent = nullptr);
+
+    void setText(const QString &text);
+    const QString &text() const { return content; }
+    bool isElided() const { return elided; }
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+signals:
+    void elisionChanged(bool elided);
+
+private:
+    bool elided;
+    QString content;
+};
